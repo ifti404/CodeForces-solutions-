0a96929363a7e6f76c7e6d409ef90b3b6de6ca4f
Md Ifti <63661869+ifti404@users.noreply.github.com>
2025-02-23 13:47:21 +0600

    Update A. Watermelon.cpp

--- A. Watermelon.cpp
+++ A. Watermelon.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >> n;
+ 
+    if (n % 2 == 1)
+    {
+        cout << "NO" << endl;
+    }
+    else if (n == 2)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES";
+    }
+    return 0;
+}
